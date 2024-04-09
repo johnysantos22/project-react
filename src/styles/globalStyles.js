423fca69f752c3d2f1be11c1a04d2b56c85f0401
johny santos <128935630+johnysantos22@users.noreply.github.com>
2024-04-08 23:17:08 -0300

    Update globalStyles.js

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -2,13 +2,23 @@ import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
 
-    *{
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        font-family: "Fira Code", monospace;
-        text-decoration: none;
-
+  *{ 
+    box-sizing: border-box ;
+    margin:0;
+    padding:0;
+    text-decoration: none;
+    .roboto-thin {
+        font-family: "Roboto", sans-serif;
+        font-weight: 100;
+        font-style: normal;
+      }
+      
+      .roboto-regular {
+        font-family: "Roboto", sans-serif;
+        font-weight: 400;
+        font-style: normal;
+      }
+      
     }
-
+`;
 `
